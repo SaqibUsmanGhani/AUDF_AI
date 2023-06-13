2aa59194a872613a46fd075b232b2f20986c5e51
SaqibUsman02 <tech54qi@gmail.com>
2023-06-13 13:19:11 +0500

    l

--- api/model.py
+++ api/model.py
@@ -77,9 +77,7 @@
 #     return "Hello World!"
 
 
-# # Running app
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -87,3 +85,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
